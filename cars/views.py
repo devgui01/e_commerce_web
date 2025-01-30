6657381153d0333f7d5b0a92a35244f879eaf3ba
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-01-30 10:02:40 -0400

    commit 30-01-25

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from cars.models import Car
 
-def cars_view(request):
+
+def cars_view(request):    
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
+         
     return render(
-        request, 
-        'cars.html',
-          {'cars': {'model': 'Astra 2.0'}}
+        request,     
+        'cars.html', 
+          {'cars': cars }
           )
-#aa
