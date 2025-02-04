25e30d96fff3371eeb512df9b07d9b7c9f5d9f81
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-02-04 10:24:05 -0400

    commit 04-02-25

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms  import CarForm
+
 
 
 def cars_view(request):    
@@ -18,10 +19,12 @@ def cars_view(request):
 
 def new_car_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)      
-        print(new_car_form.data)  
+
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
     else:
         new_car_form = CarForm()
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
-
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
 
