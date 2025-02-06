869f482d8b793124d52676e8bffc9740200ce82f
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-02-06 15:56:56 -0400

    commit 06-02-25

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 def register_view(request):
@@ -7,7 +7,7 @@ def register_view(request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('cars_list')
+            return redirect('login')
     else:  
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
@@ -24,4 +24,8 @@ def login_view(request):
             login_form = AuthenticationForm()
     else:        
         login_form = AuthenticationForm()
-    return render(request, 'login.html', {'login_form': login_form})    
+    return render(request, 'login.html', {'login_form': login_form})  
+
+def logout_view(request)  :
+    logout(request)
+    return redirect('cars_list')
