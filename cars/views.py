06c020ca9f8a9b0e3dd115845600b8f7e9390767
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-02-07 10:45:21 -0400

    commit 07-02-25

--- cars/views.py
+++ cars/views.py
@@ -3,23 +3,8 @@ from cars.models import Car
 from cars.forms  import CarModelForm
 from django.views import View
 
-
-
-def cars_view(request):    
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
-
-    if search:
-        cars = cars.filter(model__icontains=search)
-         
-    return render(
-        request,     
-        'cars.html', 
-          {'cars': cars }
-          )
-
 class CarsView(View):
-    
+
     def get(self, request):
         cars   = Car.objects.all().order_by('model')
         search = request.GET.get('search')
